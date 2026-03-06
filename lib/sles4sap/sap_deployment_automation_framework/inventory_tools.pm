a388ac5713e624eb8cc9996052af01f8297c4a49
lilyeyes <llzhao@suse.com>
2026-03-06 18:17:09 +0800

    Fix SDAF Multiple SSH key secret found issue
    TEAM-11005 - [SDAF] Multiple SSH key secret found

--- lib/sles4sap/sap_deployment_automation_framework/inventory_tools.pm
+++ lib/sles4sap/sap_deployment_automation_framework/inventory_tools.pm
@@ -13,7 +13,7 @@ use testapi;
 use Exporter qw(import);
 use Carp qw(croak);
 use sles4sap::sap_deployment_automation_framework::naming_conventions
-  qw($deployer_private_key_path $sut_private_key_path);
+  qw($deployer_private_key_path $sut_sid_private_key_path $sut_iscsi_private_key_path);
 use publiccloud::azure;
 
 =head1 SYNOPSIS
@@ -186,15 +186,23 @@ sub prepare_ssh_config {
     );
 
     # Add all SUT systems defined in inventory file
+    my $private_key_path = $sut_sid_private_key_path;
     for my $instance_type (keys(%{$args{inventory_data}})) {
         my $hosts = $args{inventory_data}->{$instance_type}{hosts};
         for my $hostname (keys %$hosts) {
             my $host_data = $hosts->{$hostname};
+            if ($hostname =~ 'iscsi') {
+                $private_key_path = $sut_iscsi_private_key_path
+            }
+            else {
+                $private_key_path = $sut_sid_private_key_path
+            }
+
             ssh_config_entry_add(
                 entry_name => "$hostname $host_data->{ansible_host}",    # This allows both hostname and IP login
                 user => $host_data->{ansible_user},
                 hostname => $host_data->{ansible_host},
-                identity_file => $sut_private_key_path,
+                identity_file => $private_key_path,
                 identities_only => 'yes',
                 proxy_jump => 'deployer_jump',
                 strict_host_key_checking => 'no'
